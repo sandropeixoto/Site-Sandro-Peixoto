5919254555a2f9f521c04a6f865dabc792e0a74d
Sandro Peixoto <contato@sandropeixoto.com.br>
2026-04-09 18:22:38 -0300

    fix: adjust relative paths for build and cleanup config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Garante caminhos relativos para ativos, essencial se o repositório for subdomínio
-  base: './', 
+  // Define o base como vazio para garantir que caminhos de build sejam relativos (./)
+  base: '', 
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
