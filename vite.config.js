661f0a45d4efe23492a5da339599ee46f924f0ec
Sandro Peixoto <contato@sandropeixoto.com.br>
2026-04-09 18:20:21 -0300

    fix: setup GitHub Actions deploy and fix blank screen by unifying JSX

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Altere para o nome do seu repositório se não for o domínio principal
-  base: '/', 
+  // Garante caminhos relativos para ativos, essencial se o repositório for subdomínio
+  base: './', 
 })
