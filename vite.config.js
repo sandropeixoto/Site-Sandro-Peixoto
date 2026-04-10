4d7a1904bf7a9b2d909dae9c45a30a0f8f55ba3d
Sandro Peixoto <contato@sandropeixoto.com.br>
2026-04-10 11:35:59 -0300

    chore: optimize deployment and fix domain CNAME

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Define o base como vazio para garantir que caminhos de build sejam relativos (./)
-  base: '', 
+  // Define o base como '/' para seu domínio customizado sandropeixoto.com.br
+  base: '/', 
   build: {
     outDir: 'dist',
     emptyOutDir: true,
